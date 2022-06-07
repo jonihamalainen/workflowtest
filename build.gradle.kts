c5913677144b468db998fc3b13dff3da870364d5
jonihamalainen <jomppe21@gmail.com>
2022-06-07 12:34:52 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-resteasy-reactive")
-    implermentation("com.github.nbaztec.coveralls-jacoco")
+    testImplementation("com.github.nbaztec.coveralls-jacoco")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
